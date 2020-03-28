478f3bfc11cb82f508a8b1dc8f0362d1c9f83fe0
Adam <galekxy13@hotmail.com>
2020-03-27 21:11:14 -0400

    updated project cards

--- adam-galek-portfolio-v2/src/Projects.js
+++ adam-galek-portfolio-v2/src/Projects.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import "./Projects.css";
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
-import photo1 from './Clouds_over_the_Atlantic_Ocean.jpg';
-import photo2 from './underwater.jpg';
+import StarWars from './StarWarsApi.PNG';
+import ComingSoon from './coming-soon-1.jpg';
 import photo3 from './waves.jpg';
 
 
@@ -14,9 +14,9 @@ class Projects extends Component {
             <div>
             <CardDeck id="CardMargins">
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Img variant="top" src={StarWars} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>StarWars API Project</Card.Title>
                     <Card.Text>
                         This is a wider card with supporting text below as a natural lead-in to
                         additional content. This content is a little bit longer.
@@ -27,7 +27,7 @@ class Projects extends Component {
                     </Card.Footer>
                 </Card>
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Img variant="top" src={ComingSoon} />
                     <Card.Body>
                     <Card.Title>Card title</Card.Title>
                     <Card.Text>
@@ -40,13 +40,16 @@ class Projects extends Component {
                     </Card.Footer>
                 </Card>
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Img variant="top" src={ComingSoon} />
                     <Card.Body>
                     <Card.Title>Card title</Card.Title>
                     <Card.Text>
                         This is a wider card with supporting text below as a natural lead-in to
                         additional content. This card has even longer content than the first to
-                        show that equal height action.
+                        show that equal height action. Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
                     </Card.Text>
                     </Card.Body>
                     <Card.Footer>
