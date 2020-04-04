f5b4a654885f1cdbc5b9f6900d79d4cb392d4c06
Adam <galekxy13@hotmail.com>
2020-04-04 14:07:17 -0400

    added youtube icon & link to contact section

--- adam-galek-portfolio-v2/src/Containers/Contact/Contact.js
+++ adam-galek-portfolio-v2/src/Containers/Contact/Contact.js
@@ -7,6 +7,7 @@ import Col from 'react-bootstrap/Col';
 import LinkedIn from './LinkedIn2.png';
 import GitHub from './GitHub128.png';
 import Email from './email128-2.png';
+import Youtube from './youtube128.png';
 
 
 
@@ -16,21 +17,26 @@ class Contact extends Component {
         return (
             <div className="contact-section" id='contact'>
 
-            <h1> Contact me </h1>
+            <h1> Contact me! </h1>
 
             <Container>
                 <Row className="d-flex justify-content-center">
-                    <Col xs="4" md="4" className="d-flex justify-content-center">
-                        <a href="https://www.linkedin.com/in/adamgalek/" target="_blank" rel="noopener noreferrer">
-                            <Image src={LinkedIn} fluid/>
-                        </a>
-                    </Col>
-                    <Col xs="4" md="4" className="d-flex justify-content-center">
+                    <Col xs="3" md="2" className="d-flex justify-content-center">
                         <a href="https://github.com/TheGalekxy" target="_blank" rel="noopener noreferrer">
                             <Image src={GitHub} fluid />
                         </a>
                     </Col>
-                    <Col xs="4" md="4" className="d-flex justify-content-center">
+                    <Col xs="3" md="2" className="d-flex justify-content-center">
+                        <a href="https://www.linkedin.com/in/adamgalek/" target="_blank" rel="noopener noreferrer">
+                            <Image src={LinkedIn} fluid/>
+                        </a>
+                    </Col>
+                    <Col xs="3" md="2" className="d-flex justify-content-center">
+                        <a href="https://www.youtube.com/channel/UCvkAweO7HT1d0gUBLX_13Ag" target="_blank" rel="noopener noreferrer">
+                            <Image src={Youtube} fluid />
+                        </a>
+                    </Col>
+                    <Col xs="3" md="2" className="d-flex justify-content-center">
                         <a href="mailto:galekxy94@gmail.com" target="_blank" rel="noopener noreferrer">
                             <Image src={Email} fluid />
                         </a>
