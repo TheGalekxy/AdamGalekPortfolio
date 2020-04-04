48f82b9c38950f40cf3ec7364846acf5844fd6a0
Adam <galekxy13@hotmail.com>
2020-04-04 18:00:33 -0400

    Updates

---
+++ precache-manifest.65aa09aea3a4708a5feddd4b6b701717.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbef6c2548f25005a2d7f5b4a46f274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a8b1902ad82b540383a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5c5889897a25d614a661",
+    "url": "/static/css/main.69de8b20.chunk.css"
+  },
+  {
+    "revision": "1a8b1902ad82b540383a",
+    "url": "/static/js/2.cc6438c5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.cc6438c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5889897a25d614a661",
+    "url": "/static/js/main.5b7dd413.chunk.js"
+  },
+  {
+    "revision": "51e60f530b7856af1a36",
+    "url": "/static/js/runtime-main.e974cfd9.js"
+  },
+  {
+    "revision": "8d95e864cdc8fa40d884b055439df15a",
+    "url": "/static/media/Adam2.8d95e864.png"
+  },
+  {
+    "revision": "4f6c6ea24964c71706ea67043b7ab614",
+    "url": "/static/media/StarWarsApi.4f6c6ea2.PNG"
+  },
+  {
+    "revision": "52f4a57b1bd501f5838bc0b4810690c7",
+    "url": "/static/media/coming-soon-1.52f4a57b.jpg"
+  }
+]);
